6433cc1c775bf69905cf5d0e38cd5d1028fad99f
Jamboy <jamboylive@gmail.com>
2021-12-03 18:31:52 +0800

    发发发

---
+++ commit.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Description:
+ * @Author: Jamboy
+ * @Date: 2021-12-03 18:26:06
+ * @LastEditTime: 2021-12-03 18:26:07
+ */
+module.exports = {
+  exports: ['@commitlint/config-conventional'],
+}
