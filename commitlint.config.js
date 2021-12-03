631fe78fa82e86a2250f0b791b74f12e4f88b571
Kivia-x <1l>
2021-12-03 21:59:13 +0800

    build: git commit verify

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Description:
+ * @Author: Jamboy
+ * @Date: 2021-12-03 21:44:24
+ * @LastEditTime: 2021-12-03 21:45:39
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+}
