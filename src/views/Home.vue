75287ee4ab6ce67be5b3bfb6a14317e60556b901
Jamboy <jamboylive@gmail.com>
2021-12-04 09:52:00 +0800

    perf: delete unuse code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,23 +2,17 @@
  * @Description:
  * @Author: Jamboy
  * @Date: 2021-12-03 21:17:42
- * @LastEditTime: 2021-12-03 21:34:45
+ * @LastEditTime: 2021-12-04 09:50:07
 -->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  components: {},
 })
 </script>
